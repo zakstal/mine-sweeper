b0a08504cd8049e06123e2653ed1852f88963429
Zachary Stallings <zakstal@gmail.com>
2016-01-11 22:40:45 -0500

    Initial Commit

---
+++ mineSweeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mineSweeper
+//
+//  Created by Zachary Stallings on 1/11/16.
+//  Copyright (c) 2016 Stallings.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
